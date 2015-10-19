f286d535e43e04a8f41e61af5b8865fdf5f843c6
chriswait <chriswait91@gmail.com>
2015-10-20 02:03:34 +1100

    adds base template, index view, root url

--- similarity/views.py
+++ similarity/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'similarity/base.html')
