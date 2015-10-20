f63fb24549d2988fcad09dafda0b8fa82bbe5f67
Eilidh Hendry <Eilidh Hendry>
2015-10-20 14:25:43 +1100

    added pos distribution, function word distribution and csv writing to preprocess

--- classifier/preprocessing.py
+++ classifier/preprocessing.py
@@ -1,11 +1,35 @@
 import string
 import nltk
-import os
+import csv
 
-def preprocess(author_name, book_title, input_filename):
-    with open(input_filename) as input_text:
-        # splits the input text into chunks and writes each chunk to a new file
-        split_text(input_text, book_title)
+def preprocess(author_name, book_title, input_dir):
+
+    # create csv writer object, output file and write headers to file
+    csv_writer = create_csv(book_title)
+
+    corpus = nltk.corpus.reader.PlaintextCorpusReader(input_dir, r'.*\.txt$')
+
+    for text_name in corpus.fileids():
+
+        # progress update
+        print text_name
+
+        # get avg_word_length, avg_sentence_length, lexical_diversity, percentage_punctuation
+        simple_stats = analyze_text(corpus, text_name)
+
+        # tag current text
+        # requires nltk maxent_treebank_tagger downloaded
+        pos_current_text = nltk.pos_tag(corpus.words(fileids=text_name))
+
+        # get normalised function word distributions
+        function_word_distribution = get_function_word_distribution(pos_current_text, corpus, text_name)
+
+        # get normalised pos distributions
+        pos_distribution = get_pos_counts(pos_current_text, corpus, text_name)
+
+        # write current text to csv file
+        csv_writer.writerow([author_name]+simple_stats+function_word_distribution+pos_distribution)
+
 
 def split_text(input_file, book_title, chunk_size=10000):
     current_chunk = []
@@ -38,33 +62,102 @@ def split_text(input_file, book_title, chunk_size=10000):
     output_file = open(book_title+"{0:02d}.txt".format(file_count),'w')
     print>>output_file, final_chunk
 
-def tag_texts(input_dir):
 
-    # takes a directory containing subdirectories for each author
-    # creates categories from subdirectory names
-    corpus = nltk.corpus.reader.CategorizedPlaintextCorpusReader(input_dir, r'.*\.txt$', cat_pattern=r'(\w+)/*')
+def analyze_text(corpus, filename):
+        chars = corpus.raw(fileids=[filename])
+        words = corpus.words(fileids=[filename])
+        sentences = corpus.sents(fileids=[filename])
+        word_count = len(words)
+        char_count = len(chars)
+        sentence_count = len(sentences)
+        vocab_count = len(set(w.lower() for w in corpus.words(fileids=[filename])))
+        punctuation_count = count_punctuation(corpus.raw(fileids=[filename]))
+        avg_word_length = float(char_count)/word_count
+        avg_sentence_length = float(word_count)/sentence_count
+        lexical_diversity = float(vocab_count) / word_count
+        percentage_punctuation = float(punctuation_count) / char_count
+        return [avg_word_length, avg_sentence_length, lexical_diversity, percentage_punctuation]
 
-    # try to create the output directory
-    try:
-        os.mkdir('tagged_dir')
-    except OSError:
-        # already exists so skip
-        pass
 
-    # loop through all the categories (authors)
-    for category in corpus.categories():
+def count_punctuation(raw_corpus):
+    punctuation_marks = ['!',',','.',':','"','?','-',';','(',')','[',']','\\','/']
+    punc = [char for char in raw_corpus if char in punctuation_marks]
+    return len(punc)
 
-        # loop through all the files (texts) in the current category (authors)
-        for filename in corpus.fileids(category):
 
-            # print current text to terminal
-            print filename
+def get_pos_counts(tagged_text, corpus, filename):
+    # TODO: check should be normalised by no. words or no. tags
+    tag_list = ['PRP$', 'VBG', 'VBD', 'VBN', 'POS', 'VBP', 'WDT', 'JJ', 'WP', 'VBZ', 'DT', 'RP', 'NN', 'FW', 'TO',
+                 'PRP', 'RB', 'NNS', 'NNP', 'VB', 'WRB', 'CC', 'LS', 'PDT', 'RBS', 'RBR', 'CD', 'EX',
+                 'IN', 'WP$', 'MD', 'NNPS', 'JJS', 'JJR', 'UH']
+    # initialise dictionary with tag list as keys
+    final_pos_distribution = {key: 0 for key in tag_list}
 
-            output_file = open('tagged_dir/'+filename, 'w')
+    # create frequency distribution of tags in text
+    tagged_text_freq_dist = nltk.FreqDist(tag for (word, tag) in tagged_text)
 
-            pos_corpus = nltk.pos_tag(corpus.words(fileids=filename))
-            print>>output_file, pos_corpus
+    # find the length of the current text
+    text_length = len(corpus.words(fileids=filename))
 
-if __name__== '__main__':
-    with open('README.md') as input_file:
-        print split_text(input_file, 'README', chunk_size=10)
+    # loop through the frequency distribution dictionary, normalise and add to empty but initialised dictionary of tags
+    # this approach preserves 0 values for tags which do not occur in current text
+    for tag, count in tagged_text_freq_dist.iteritems():
+        final_pos_distribution[tag] = float(count)/text_length
+
+    # create list of results in order to preserve tag list ordering
+    ordered_pos_distributions = [final_pos_distribution[tag] for tag in tag_list]
+    return ordered_pos_distributions
+
+
+def get_function_word_distribution(tagged_text, plain_corpus, text_name):
+    # TODO: check normalised by no. words or no. tags
+    taglist = 'PRP PRP$ WP WP$ CC MD UH RP IN TO WDT DT PDT'.split()
+
+    word_fd = nltk.FreqDist(word.lower() for (word, tag) in tagged_text if tag in taglist)
+
+    word_list = ['the', 'and', 'of', 'a', 'to', 'in', 'i', 'he', 'it', 'that', 'you', 'his', 'with', 'on', 'for', 'at',
+                 'as', 'but', 'her', 'they', 'she', 'him', 'all', 'this', 'we', 'from', 'or', 'out', 'an', 'my', 'by',
+                 'up', 'what', 'me', 'no', 'like', 'would', 'if', 'about', 'which', 'them', 'into', 'who', 'could',
+                 'can', 'some', 'their', 'over', 'down', 'your', 'will', 'its', 'any', 'through', 'after', 'off', 'than',
+                 'our', 'us', 'around', 'these', 'because', 'must', 'before', 'those', '&', 'should', 'himself', 'both',
+                 'against', 'may', 'might', 'shall', 'since', 'de', 'within', 'between', 'each', 'under', 'until', 'toward',
+                 'another', 'myself']
+
+    # initialise dictionary with word list as keys
+    word_list_dict = {key: 0 for key in word_list}
+
+    text_length = len(plain_corpus.words(fileids=text_name))
+
+    for word, count in word_fd.iteritems():
+        if word in word_list:
+            word_list_dict[word] = float(count)/text_length
+
+    # create list of results in order to preserve function word list ordering
+    ordered_func_word_distributions = [word_list_dict[word] for word in word_list]
+
+    return ordered_func_word_distributions
+
+
+def create_csv(book_title):
+    fieldnames = ['target', 'avg_word_length', 'avg_sentence_length', 'lexical_diversity', 'percentage_punctuation',
+                  'the', 'and', 'of', 'a', 'to', 'in', 'i', 'he', 'it', 'that', 'you', 'his', 'with', 'on', 'for', 'at',
+                  'as', 'but', 'her', 'they', 'she', 'him', 'all', 'this', 'we', 'from', 'or', 'out', 'an', 'my', 'by',
+                  'up', 'what', 'me', 'no', 'like', 'would', 'if', 'about', 'which', 'them', 'into', 'who', 'could',
+                  'can', 'some', 'their', 'over', 'down', 'your', 'will', 'its', 'any', 'through', 'after', 'off', 'than',
+                  'our', 'us', 'around', 'these', 'because', 'must', 'before', 'those', '&', 'should', 'himself', 'both',
+                  'against', 'may', 'might', 'shall', 'since', 'de', 'within', 'between', 'each', 'under', 'until', 'toward',
+                  'another', 'myself', 'PRP$', 'VBG', 'VBD', 'VBN', 'POS', 'VBP', 'WDT', 'JJ', 'WP', 'VBZ', 'DT', 'RP',
+                  'NN', 'FW', 'TO', 'PRP', 'RB', 'NNS', 'NNP', 'VB', 'WRB', 'CC', 'LS', 'PDT', 'RBS', 'RBR', 'CD', 'EX',
+                  'IN', 'WP$', 'MD', 'NNPS', 'JJS', 'JJR', 'UH']
+
+    # create output file in output folder, with name of input folder
+    output_file = open('temp/fingerprint_output/'+book_title+'.csv', 'w')
+
+    # create csv writer object and write the fieldnames to first row
+    csv_writer = csv.writer(output_file, delimiter='\t')
+    csv_writer.writerow(fieldnames)
+    return csv_writer
+
+if __name__ == '__main__':
+
+    preprocess('hemingway', 'acrosstheriverandintothetrees', 'test_data/acrosstheriverandintothetrees')
