fc23dcdea82e293257f30d270700a3bbf479b70f
Eilidh Hendry <Eilidh Hendry>
2015-12-13 15:10:52 +1100

    joining punctuation to the previous word

--- similarity/classifier/chunk.py
+++ similarity/classifier/chunk.py
@@ -37,8 +37,10 @@ def chunk_text(input_path, author, title, chunk_size=10000):
     current_chunk_word_count = 0
     file_count = 0
     for sentence in sentences:
+        # TODO: Don't just remove non-ascii
         # add the sentence to the current chunk and remove non-ascii characters
         current_chunk = current_chunk + [word.encode('ascii', 'ignore') for word in sentence]
+
         for word in sentence:
 
             # increment the word count if not whitespace
@@ -47,8 +49,11 @@ def chunk_text(input_path, author, title, chunk_size=10000):
 
             if current_chunk_word_count == chunk_size:
 
-                # convert list of words to string
-                chunk = ' '.join(current_chunk)
+                # first join some punctuation to the previous word (returns generator object)
+                punctuation_joined_chunk = join_punctuation(current_chunk)
+
+                # then convert list of words to string
+                chunk = ' '.join(punctuation_joined_chunk)
 
                 # create new file in the output directory with 0 padding
                 output_file = open(chunk_output_directory+"{0:03d}.txt".format(file_count),'w')
@@ -66,8 +71,17 @@ def chunk_text(input_path, author, title, chunk_size=10000):
     output_file = open(chunk_output_directory+"{0:03d}.txt".format(file_count),'w')
     print>>output_file, final_chunk
 
-if __name__ == '__main__':
-    author = 'hemingway'
-    title = 'across the river and into the trees'
-    text_output_path = generate_text_path(author, title)
-    chunk_text('temp/acrosstheriverandintothetrees.txt', author, title, chunk_size=10000)
+
+def join_punctuation(word_list):
+    # note only includes the punctuation marks you want to join to prev word
+    punctuation_marks = ['!', ',', '.', ':', '"', '\'', '?', ';']
+    iter_word_list = iter(word_list)
+    current_word = next(iter_word_list)
+
+    for next_word in iter_word_list:
+        if next_word in punctuation_marks:
+            current_word += next_word
+        else:
+            yield current_word
+            current_word = next_word
+    yield current_word
