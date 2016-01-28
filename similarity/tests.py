847eaf86ba660202a485a5ce1db8697b499b8765
chriswait <chriswait91@gmail.com>
2016-01-28 22:41:05 +1100

    adds example test case
    test for syllable - 3 tests, 2 currently failing

--- similarity/tests.py
+++ similarity/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from classifier.compute_fingerprint import avg_syllables
 
 # Create your tests here.
+class SyllableTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_no_syllables_in_string(self):
+        test_sentence = "the quick brown fox jumped over the lazy dog"
+        syllables_freq = avg_syllables(test_sentence.split())
+        probably_right = 11.0/9
+        self.assertEqual(syllables_freq, probably_right)
+
+    def test_no_syllables_in_empty_string(self):
+        test_sentence = ""
+        syllables_freq = avg_syllables(test_sentence.split())
+        probably_right = 0.0
+        self.assertEqual(syllables_freq, probably_right)
+
+    def test_no_syllables_in_arbitrary_words(self):
+        test_sentence = "floogly rhombile shanaynord"
+        syllables_freq = avg_syllables(test_sentence.split())
+        probably_right = 0.0
+        self.assertEqual(syllables_freq, probably_right)
