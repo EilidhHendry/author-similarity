11af44d3218debf6c336ecad593c31bafc679912
chriswait <chriswait91@gmail.com>
2016-02-22 00:27:20 +1100

    adds route for getting chunks

--- similarity/views.py
+++ similarity/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from models import Chunk
 
 
 def index(request):
     return render(request, 'similarity/base.html')
+
+def all_chunks(request):
+    chunks = Chunk.objects.all()
+    authors = []
+    fingerprints = []
+    for chunk in chunks:
+        authors.append(chunk.author.name)
+        fingerprints.append(chunks[0].get_fingerprint())
+    result = {
+        "authors": authors,
+        "fingerprints": fingerprints,
+    }
+    response = JsonResponse(result)
+    return response
