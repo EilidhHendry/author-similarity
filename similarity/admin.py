dc408aece72798f05ce0cba50abe533cb1c5fe11
chriswait <chriswait91@gmail.com>
2016-03-29 16:32:30 +1100

    adds list_display fields to model admins

--- similarity/admin.py
+++ similarity/admin.py
@@ -2,19 +2,26 @@ from django.contrib import admin
 from .models import Author, Text, Chunk, Classifier
 from classifier import svm
 
-admin.site.register(Chunk)
+
+class ChunkAdmin(admin.ModelAdmin):
+    list_display = ['__unicode__', 'text', 'author']
+
+admin.site.register(Chunk, ChunkAdmin)
+
 
 class ChunkInline(admin.StackedInline):
     model = Chunk
 
 class TextAdmin(admin.ModelAdmin):
     actions = ['process_text']
+    list_display = ['__unicode__', 'author']
     def process_text(self, request, queryset):
         for selected_text in queryset:
             selected_text.delete()
             selected_text.save()
 
 admin.site.register(Text, TextAdmin)
+
 
 class TextInline(admin.StackedInline):
     model = Text
@@ -26,12 +33,14 @@ class AuthorAdmin(admin.ModelAdmin):
 
 admin.site.register(Author, AuthorAdmin)
 
+
 class ClassifierAdmin(admin.ModelAdmin):
     actions = ['train_classifier', 'find_accuracy']
+    list_display = ['__unicode__', 'status']
     def train_classifier(self, request, queryset):
         classifier = queryset[0]
         classifier.train()
-    train_classifier.short_description = "Train Classifier"
+
     def find_accuracy(self, request, queryset):
         print "Fetching Chunks"
         chunks = Chunk.objects.all()
