50ba58a57c164af3aad7b71a6be1dd2030655340
chriswait <chriswait91@gmail.com>
2016-05-10 20:14:28 +1000

    front-end - angular, stylin, template & view tweaks
    adds basic angularJS app to ajax classification
    adds style reset for textareas
    modifies base template to use angular bindings
    removes form processing from index view
    adds csrf exemption to classify view

--- app/app.js
+++ app/app.js
@@ -1,4 +1,44 @@
-setTimeout( function() {
-    document.querySelector('#text').focus();
-    console.log("Hello, world!");
+var apiRoot = "/";
+var classifyUrl = apiRoot + "classify";
+
+var app = angular.module("author", ['ngMaterial']);
+
+app.controller('MainController', function($scope, $document, $timeout, $http, $log) {
+
+    var textarea = document.querySelector('#text');
+    // TODO: handle file uploads
+    //var fileUpload = document.querySelector('#file');
+
+    $scope.dataModel = {
+        text: "test",
+        classification: undefined,
+    };
+
+    $scope.classify = function() {
+        var data = {
+            text: $scope.dataModel.text
+        };
+        $http.post(classifyUrl, data)
+        .then(function(successData) {
+            var classificationData;
+            if ("data" in successData) {
+                classificationData = {};
+                if ("authors" in successData["data"]) {
+                    classificationData["authors"] = successData["data"]["authors"];
+                }
+                if ("fingerprint" in successData["data"]) {
+                    classificationData["fingerprint"] = successData["data"]["fingerprint"];
+                }
+                $scope.dataModel.classification = classificationData;
+                $log.log($scope.dataModel.classification);
+            }
+        }, function(errorData) {
+            $log.log(errorData);
+        });
+    }
+
+    // TODO: this more sensibly
+    $timeout(function() {
+        angular.element(textarea.focus());
+    })
 });
--- similarity/views.py
+++ similarity/views.py
@@ -1,25 +1,15 @@
-from similarity.classifier import clean_up
-
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from models import Chunk, Classifier
 from forms import InputForm
 
-def index(request):
-    text = ""
-    if request.method == 'POST':
-        form = InputForm(request.POST, request.FILES)
-        if form.is_valid():
-            if form.cleaned_data.get('input_file'):
-                input_file = form.cleaned_data.get('input_file')
-                text = input_file.read()
-            elif form.cleaned_data.get('text'):
-                text = form.cleaned_data.get('text')
-            return classify_text(text)
-    else:
-        form = InputForm(initial={'text': 'text here'})
-    return render(request, 'similarity/base.html', {'form': form})
+from similarity.classifier import clean_up
 
+def index(request):
+    return render(request, 'similarity/base.html')
+
+@csrf_exempt
 def classify(request):
     text = ""
     text_key = 'text'
@@ -27,9 +17,6 @@ def classify(request):
         text = str(request.POST[text_key])
     if (text_key in request.GET):
         text = str(request.GET[text_key])
-    return classify_text(text)
-
-def classify_text(text):
     system_classifier = Classifier.objects.first()
     result = {}
     result = system_classifier.classify(text)
